a226e13470cc76ae0dd6280f027114140a820955
NachoToast <32235595+NachoToast@users.noreply.github.com>
2025-06-14 13:28:44 +1200

    v1.6

--- Source/AssignTabReducer/Main.cs
+++ Source/AssignTabReducer/Main.cs
@@ -1,48 +1,46 @@
 ﻿using HarmonyLib;
 using RimWorld;
-using System.Collections.Generic;
 using Verse;
 
-namespace AssignTabReducer
+namespace AssignTabReducer;
+
+[StaticConstructorOnStartup]
+public static class AssignTabReducer
 {
-    [StaticConstructorOnStartup]
-    public static class AssignTabReducer
+    static AssignTabReducer()
     {
-        static AssignTabReducer()
-        {
-            Harmony harmony = new Harmony("NachoToast.AssignTabReducer");
+        Harmony harmony = new("NachoToast.AssignTabReducer");
 
-            harmony.Patch(
-                original: AccessTools.PropertyGetter(
-                    type: typeof(MainTabWindow_Assign),
-                    name: "Pawns"),
-                postfix: new HarmonyMethod(
-                    methodType: typeof(AssignTabReducer),
-                    methodName: nameof(GetAssignPawns_Postfix)));
+        harmony.Patch(
+            original: AccessTools.PropertyGetter(
+                type: typeof(MainTabWindow_Assign),
+                name: "Pawns"),
+            postfix: new HarmonyMethod(
+                methodType: typeof(AssignTabReducer),
+                methodName: nameof(GetAssignPawns_Postfix)));
+    }
+
+    private static void GetAssignPawns_Postfix(ref IEnumerable<Pawn> __result)
+    {
+        Map currentMap = Find.CurrentMap;
+
+        if (currentMap == null)
+        {
+            return;
         }
 
-        private static void GetAssignPawns_Postfix(ref IEnumerable<Pawn> __result)
+        List<Pawn> newResult = [];
+
+        foreach (Pawn pawn in __result)
         {
-            Map currentMap = Find.CurrentMap;
+            Map pawnMap = pawn?.Map;
 
-            if (currentMap == null)
+            if (pawnMap == null || pawnMap == currentMap)
             {
-                return;
+                newResult.Add(pawn);
             }
-
-            List<Pawn> newResult = new List<Pawn>();
-
-            foreach (Pawn pawn in __result)
-            {
-                Map pawnMap = pawn?.Map;
-
-                if (pawnMap == null || pawnMap == currentMap)
-                {
-                    newResult.Add(pawn);
-                }
-            }
-
-            __result = newResult;
         }
+
+        __result = newResult;
     }
 }
